dc0a8341eff473ff801b1d108717eab6668c95f3
Matheus Hoffmann <mmatheushb@hotmail.com>
2021-03-05 08:06:19 -0300

    Car parameters class

---
+++ SUSPENSION_VibrationAnalysis/Parameters.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, driver_mass=70.0, car_mass=180.0, total_mass=250.0, wheelbase=1.6, front_wb2CG=0.9):
+        """
+        BASIC CAR PARAMETERS
+        :param driver_mass: driver mass [kg]
+        :param car_mass: car mass [kg]
+        :param total_mass: driver + car masses [kg]
+        :param wheelbase: distance between the front and the rear wheel center [m]
+        :param front_wb2CG: distance between the front wheel center and the car CG [m]
+        """
+
+class Suspension:
+    def __init__(self):
